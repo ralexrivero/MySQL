6637b6fc6a324c4812b7eae1ad6bca5bd29bb234
ralexrivero <ralexrivero@gmail.com>
2023-01-05 04:05:34 -0300

    use parenthesis

--- mysql_advanced/lucky_lab/00-filtering_data.sql
+++ mysql_advanced/lucky_lab/00-filtering_data.sql
@@ -2,3 +2,4 @@ SELECT * FROM Orders WHERE Cost <= 250 ORDER BY Cost DESC;
 SELECT * FROM Orders WHERE Cost BETWEEN 50 AND 750 ORDER BY Cost ASC;
 SELECT * FROM Orders WHERE ClientID='Cl3' AND Cost > 100 ORDER BY Cost DESC;
 SELECT * FROM Orders WHERE ProductID IN ('p1','p2') AND Quantity > 2 ORDER BY Cost DESC;
+SELECT * FROM Orders WHERE productID = 'P3' AND (ClientID = 'Cl2' OR ClientID ='Cl3');
