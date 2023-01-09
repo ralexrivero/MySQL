edceabff44014e40c067a57ff6f9c8e40a664e44
ralexrivero <ralexrivero@gmail.com>
2023-01-09 00:37:31 -0300

    order by

---
+++ mysql_advanced/05-GROUP_BY/00-create_schema.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS lucky_shrub;
+USE lucky_shrub;
+CREATE TABLE IF NOT EXISTS Orders (OrderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, Departament VARCHAR(50), OrderDate DATE, OrderQty INT, OrderTotal INT);
