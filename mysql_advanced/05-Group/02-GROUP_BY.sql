7a1ab34fd7ecc201666ed61aeb4ba627be8f6a17
ralexrivero <ralexrivero@gmail.com>
2023-01-10 11:12:11 -0300

    fix indent

--- mysql_advanced/05-Group/02-GROUP_BY.sql
+++ mysql_advanced/05-Group/02-GROUP_BY.sql
@@ -1,3 +1,3 @@
 SELECT Departament, COUNT(OrderID) AS 'Lines', SUM(OrderTotal) AS U$S FROM Orders GROUP BY Departament ORDER BY U$S DESC;
 
-    SELECT Departament, COUNT(OrderID) AS 'Lines', SUM(OrderQty) AS 'Total Qty', ROUND(AVG(OrderQty)) 'Avg Qty', MIN(OrderTotal) AS 'Min U$S', MAX(OrderTotal) AS 'Max U$S', ROUND(AVG(OrderTotal)) AS 'Avg U$S', ROUND(AVG(OrderTotal)/COUNT(OrderID)) AS 'Avg Ticket', SUM(OrderTotal) AS U$S FROM Orders GROUP BY Departament ORDER BY U$S DESC;
+SELECT Departament, COUNT(OrderID) AS 'Lines', SUM(OrderQty) AS 'Total Qty', ROUND(AVG(OrderQty)) 'Avg Qty', MIN(OrderTotal) AS 'Min U$S', MAX(OrderTotal) AS 'Max U$S', ROUND(AVG(OrderTotal)) AS 'Avg U$S', ROUND(AVG(OrderTotal)/COUNT(OrderID)) AS 'Avg Ticket', SUM(OrderTotal) AS U$S FROM Orders GROUP BY Departament ORDER BY U$S DESC;
