b5a7f41f45c525564c75b368b3a4e2e5aef88a9a
ralexrivero <ralexrivero@gmail.com>
2023-01-11 00:53:03 -0300

    HAVING clause with ALIAS

--- mysql_advanced/05-Group/03-HAVING.sql
+++ mysql_advanced/05-Group/03-HAVING.sql
@@ -1,1 +1,2 @@
 SELECT Department, SUM(OrderTotal) FROM Orders GROUP BY Department HAVING SUM(OrderTotal) > 12500 ORDER BY SUM(OrderTotal) DESC;
+SELECT Department, SUM(OrderQty) AS Qty, SUM(OrderTotal) AS Total FROM Orders GROUP BY Department HAVING Total > 12500 ORDER BY Total;
