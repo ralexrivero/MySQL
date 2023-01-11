d9577f757647f51e7b8e3ac44d9862b71904ddee
ralexrivero <ralexrivero@gmail.com>
2023-01-11 01:29:36 -0300

    fix dates, add HAVING clauses

--- mysql_advanced/05-Group/03-HAVING.sql
+++ mysql_advanced/05-Group/03-HAVING.sql
@@ -1,2 +1,4 @@
 SELECT Department, SUM(OrderTotal) FROM Orders GROUP BY Department HAVING SUM(OrderTotal) > 12500 ORDER BY SUM(OrderTotal) DESC;
 SELECT Department, SUM(OrderQty) AS Qty, SUM(OrderTotal) AS Total FROM Orders GROUP BY Department HAVING Total > 12500 ORDER BY Total;
+SELECT OrderDate, COUNT(OrderID) FROM Orders GROUP BY OrderDate;
+SELECT Department, SUM(OrderQty) AS Qty FROM Orders GROUP BY Department;
