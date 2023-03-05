35e415ded6b5290f6696278d32ee36da05969563
ralexrivero <ralexrivero@gmail.com>
2023-03-04 22:28:24 -0300

    create db and table

---
+++ mysql_advanced/11-virtual_tables/11-00-create_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE hatorshop_db;
+USE hatorshop_db;
+
+CREATE TABLE Products (
+  ProductID INT AUTO_INCREMENT,
+  Name VARCHAR(200),
+  Price INT,
+  PRIMARY KEY (ProductID),
+  UNIQUE(Name),
+  CHECK (PRICE > 0)
+)
