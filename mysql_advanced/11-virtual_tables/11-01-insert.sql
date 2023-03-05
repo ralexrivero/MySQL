3ef5207572d479c74c3573a53bcaed38e6c5078c
ralexrivero <ralexrivero@gmail.com>
2023-03-04 22:39:36 -0300

    insert data

---
+++ mysql_advanced/11-virtual_tables/11-01-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO Products(Name, Price) VALUES
+('Bell peper', 14.5),
+('Garlic', 28.15),
+('Onion', 18.29),
+('Carrot', 25.15),
+('Peppers', 15.5),
+('Tomato', 30.15),
+('Leek', 7.18),
+('Pumpkin', 30.0),
+('Basil', 15.18),
+('Ginger', 37);
